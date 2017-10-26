cd225d331aad46e232814812e2d9eb763e174db1
Fotis Evangelou <fevangelou@users.noreply.github.com>
2017-10-26 13:21:12 +0300

    Slight update to allow switching vhost generation interval easily.

--- nginx/utilities/https_vhosts.sh
+++ nginx/utilities/https_vhosts.sh
@@ -9,21 +9,22 @@
 #  * @license    GNU/GPL license: https://www.gnu.org/copyleft/gpl.html
 #  */
 
+INTERVAL="15" # Interval in seconds, must not exceed 60 (seconds)
 COUNTER="0"
 
 function generate_https_vhosts {
-	if [ -f /etc/nginx/utilities/https_vhosts.php ]; then
-		RUN_CHECK=$(/usr/bin/php -c /dev/null /etc/nginx/utilities/https_vhosts.php)
-		if [[ $RUN_CHECK == 1 ]]; then
-			bash /usr/local/src/engintron/engintron.sh purgecache
-		fi
-	fi
-	sleep 15
+    if [ -f /etc/nginx/utilities/https_vhosts.php ]; then
+        RUN_CHECK=$(/usr/bin/php -c /dev/null /etc/nginx/utilities/https_vhosts.php)
+        if [[ $RUN_CHECK == 1 ]]; then
+            bash /usr/local/src/engintron/engintron.sh purgecache
+        fi
+    fi
 }
 
-while [ $COUNTER -lt 3 ]; do
-	generate_https_vhosts
-	COUNTER=$[$COUNTER+1]
+while [ $COUNTER -lt 60 ]; do
+    generate_https_vhosts
+    sleep $INTERVAL
+    COUNTER=$[$COUNTER+$INTERVAL]
 done
 
 exit 0
