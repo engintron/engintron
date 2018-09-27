2fde64393e5d3a37e20cae3d9f57cf4e58e7d609
Luca Pasquali <lpasquali@gmail.com>
2018-09-27 10:45:18 +0200

    Fix to minimize critical errors because files are not found because being deleted by function purgecache, see the issue reported here
    https://github.com/engintron/engintron/issues/933#issue-364365217

--- engintron.sh
+++ engintron.sh
@@ -956,9 +956,6 @@ purgecache)
     echo ""
     echo "--- Process started at $NOW ---"
     echo ""
-    find /var/cache/nginx/engintron_dynamic/ -type f | xargs rm -rvf
-    find /var/cache/nginx/engintron_static/ -type f | xargs rm -rvf
-    find /var/cache/nginx/engintron_temp/ -type f | xargs rm -rvf
     if [ "$(pstree | grep 'httpd')" ]; then
         echo "Restarting Apache..."
         /scripts/restartsrv apache_php_fpm
@@ -966,6 +963,9 @@ purgecache)
         echo ""
     fi
     if [ "$(pstree | grep 'nginx')" ]; then
+        find /var/cache/nginx/engintron_dynamic/ -type f | xargs rm -rvf
+        find /var/cache/nginx/engintron_static/ -type f | xargs rm -rvf
+        find /var/cache/nginx/engintron_temp/ -type f | xargs rm -rvf
         echo "Restarting Nginx..."
         service nginx restart
         echo ""
